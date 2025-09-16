f6f90a703518ffe3883b3c9db17fce32c3fc0250
nohojong <shghwhs123@gmail.com>
2025-09-16 16:51:53 +0900

    3nd

--- counsel-back/src/main/java/io/notfound/counsel_back/auth/repository/RefreshTokenRepository.java
+++
@@ -1,7 +0,0 @@
-//package io.notfound.counsel_back.auth.repository;
-//
-//import io.notfound.counsel_back.auth.entity.RefreshToken;
-//import org.springframework.data.jpa.repository.JpaRepository;
-//
-//public interface RefreshTokenRepository extends JpaRepository<RefreshToken, String> {
-//}
