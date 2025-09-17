2972d90aeafbd79fa51865396dc26ed03a96833c
YunaKT <uijk1213@gmail.com>
2025-09-17 15:42:51 +0900

    s3 modified

--- counsel-back/src/main/java/io/notfound/counsel_back/board/config/S3Config.java
+++ counsel-back/src/main/java/io/notfound/counsel_back/board/config/S3Config.java
@@ -1,11 +1,13 @@
 package io.notfound.counsel_back.board.config;
-import com.amazonaws.auth.AWSStaticCredentialsProvider;
-import com.amazonaws.auth.BasicAWSCredentials;
-import com.amazonaws.services.s3.AmazonS3;
-import com.amazonaws.services.s3.AmazonS3ClientBuilder;
+
+
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import software.amazon.awssdk.auth.credentials.AwsBasicCredentials;
+import software.amazon.awssdk.auth.credentials.StaticCredentialsProvider;
+import software.amazon.awssdk.regions.Region;
+import software.amazon.awssdk.services.s3.S3Client;
 
 @Configuration
 public class S3Config {
@@ -20,11 +22,13 @@ public class S3Config {
     private String region;
 
     @Bean
-    public AmazonS3 amazonS3() {
-        BasicAWSCredentials awsCreds = new BasicAWSCredentials(accessKey, secretKey);
-        return AmazonS3ClientBuilder.standard()
-                .withRegion(region)
-                .withCredentials(new AWSStaticCredentialsProvider(awsCreds))
+    public S3Client s3Client() {
+        // AwsBasicCredentials을 사용하여 자격 증명을 설정합니다.
+        AwsBasicCredentials awsCreds = AwsBasicCredentials.create(accessKey, secretKey);
+
+        return S3Client.builder()
+                .region(Region.of(region))  // Region 설정
+                .credentialsProvider(StaticCredentialsProvider.create(awsCreds))  // 자격 증명 제공자 설정
                 .build();
     }
 }
--- counsel-back/src/main/java/io/notfound/counsel_back/board/service/S3Service.java
+++ counsel-back/src/main/java/io/notfound/counsel_back/board/service/S3Service.java
@@ -1,22 +1,24 @@
 package io.notfound.counsel_back.board.service;
 
-import com.amazonaws.services.s3.AmazonS3;
-import com.amazonaws.services.s3.model.CannedAccessControlList;
-import com.amazonaws.services.s3.model.ObjectMetadata;
-import com.amazonaws.services.s3.model.PutObjectRequest;
+
 import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 import org.springframework.web.multipart.MultipartFile;
+import software.amazon.awssdk.core.sync.RequestBody;
+import software.amazon.awssdk.services.s3.S3Client;
+import software.amazon.awssdk.services.s3.model.ObjectCannedACL;
+import software.amazon.awssdk.services.s3.model.PutObjectRequest;
 
 import java.io.IOException;
 import java.util.UUID;
 
 @Service
 @RequiredArgsConstructor
+
 public class S3Service {
 
-    private final AmazonS3 amazonS3;
+    private final S3Client s3Client;  // AmazonS3 대신 S3Client 사용
 
     @Value("${cloud.aws.s3.bucket}")
     private String bucket;
@@ -25,18 +27,20 @@ public class S3Service {
         String fileName = UUID.randomUUID() + "_" + file.getOriginalFilename();
 
         try {
-            ObjectMetadata metadata = new ObjectMetadata();
-            metadata.setContentLength(file.getSize());
-            metadata.setContentType(file.getContentType());
+            // 파일 업로드를 위한 PutObjectRequest 생성
+            PutObjectRequest putObjectRequest = PutObjectRequest.builder()
+                    .bucket(bucket)
+                    .key(fileName)
+                    .acl(ObjectCannedACL.PUBLIC_READ)  // Public 읽기 권한 설정
+                    .build();
 
-            amazonS3.putObject(
-                    new PutObjectRequest(bucket, fileName, file.getInputStream(), metadata)
-                            .withCannedAcl(CannedAccessControlList.PublicRead)
-            );
+            // 파일을 RequestBody로 변환하여 업로드
+            s3Client.putObject(putObjectRequest, RequestBody.fromInputStream(file.getInputStream(), file.getSize()));
         } catch (IOException e) {
             throw new RuntimeException("S3 업로드 실패", e);
         }
 
-        return amazonS3.getUrl(bucket, fileName).toString();
+        // 업로드된 파일의 URL을 반환
+        return s3Client.utilities().getUrl(builder -> builder.bucket(bucket).key(fileName)).toExternalForm();
     }
 }
