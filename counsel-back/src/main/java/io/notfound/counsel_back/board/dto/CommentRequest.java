763e7105991e5c62fedef2a573699d4c957dba39
Park Shinhee <mam2zyo@gmail.com>
2025-09-23 17:48:27 +0900

    roll back board backend

--- counsel-back/src/main/java/io/notfound/counsel_back/board/dto/CommentRequest.java
+++ counsel-back/src/main/java/io/notfound/counsel_back/board/dto/CommentRequest.java
@@ -1,6 +1,5 @@
 package io.notfound.counsel_back.board.dto;
 
-import jakarta.validation.constraints.NotBlank;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -11,13 +10,5 @@ import lombok.Setter;
 @NoArgsConstructor
 @AllArgsConstructor
 public class CommentRequest {
-
-    @NotBlank(message = "내용은 필수 입력값입니다.")
     private String content;
-
-    @NotBlank(message = "작성자 이름은 필수 입력값입니다.")
-    private String writerName;
-
-    @NotBlank(message = "비밀번호는 필수 입력값입니다.")
-    private String password;
 }
--- counsel-back/src/main/java/io/notfound/counsel_back/board/repository/ReportRepository.java
+++
@@ -1,7 +0,0 @@
-package io.notfound.counsel_back.board.repository;
-
-import io.notfound.counsel_back.board.entity.Report;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface ReportRepository extends JpaRepository<Report, Long> {
-}
